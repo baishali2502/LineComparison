a432bd3e115310b2ed34431875af98d151eff908
baishali2502 <baishali2502@gmail.com>
2023-11-23 08:25:58 +0530

    Welcome

---
+++ LineComparison/src/LineComparison/LineComparison.java
@@ -0,0 +1,20 @@
+package LineComparison;
+import java.util.*;
+
+public class LineComparison {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the x-coordinate of the first point: ");
+        double x1 = scanner.nextDouble();
+        System.out.print("Enter the y-coordinate of the first point: ");
+        double y1 = scanner.nextDouble();
+        System.out.print("Enter the x-coordinate of the second point: ");
+        double x2 = scanner.nextDouble();
+        System.out.print("Enter the y-coordinate of the second point: ");
+        double y2 = scanner.nextDouble();
+	}
+
+}
