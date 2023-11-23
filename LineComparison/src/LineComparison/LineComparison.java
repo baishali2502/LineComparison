e827dd64842916adaa56c492c7f0b941a6a50d18
baishali2502 <baishali2502@gmail.com>
2023-11-23 08:36:56 +0530

    Checked if 2 lines are equal or not

--- LineComparison/src/LineComparison/LineComparison.java
+++ LineComparison/src/LineComparison/LineComparison.java
@@ -6,7 +6,15 @@ public class LineComparison
 	public static double lineLength(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
-
+	private static void isEqual(double x1, double y1, double x2, double y2, double x3, double y3, double x4,
+			double y4) {
+		double len1 = lineLength(x1,y1,x2,y2);
+		double len2 = lineLength(x3,y3,x4,y4);
+		if(len1==len2)
+			System.out.println("Lines are equal");
+		else
+			System.out.println("Lines are not equal");	
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Line Comparison Computation Program");
@@ -20,6 +28,17 @@ public class LineComparison
         System.out.print("Enter the y-coordinate of the second point: ");
         double y2 = scanner.nextDouble();
         System.out.println("Line Length = "+lineLength(x1,y1,x2,y2));
+        System.out.print("Enter the x-coordinate of the 3rd point: ");
+        double x3 = scanner.nextDouble();
+        System.out.print("Enter the y-coordinate of the 3rd point: ");
+        double y3 = scanner.nextDouble();
+        System.out.print("Enter the x-coordinate of the 4rth point: ");
+        double x4 = scanner.nextDouble();
+        System.out.print("Enter the y-coordinate of the 4rth point: ");
+        double y4 = scanner.nextDouble();
+        isEqual(x1,y1,x2,y2,x3,y3,x4,y4);
 	}
+
+	
 
 }
